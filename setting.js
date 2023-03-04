fed0de0645b33d7f82d98859699dbdfa94100920
monaulic <sohelmuhammad164@gmail.com>
2023-03-04 12:05:36 +0600

    Docker Test

---
+++ setting.js
@@ -0,0 +1,6 @@
+const classCatch = document.getElementsByClassName("root");
+
+const h1Create = document.createElement("h1");
+h1Create.classList.add("headingTitle");
+
+classCatch.appendChild(h1Create);
