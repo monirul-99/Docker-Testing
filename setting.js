5badd6923a21d036654090fe56a6b2f4bc9d27f0
monaulic <sohelmuhammad164@gmail.com>
2023-03-04 12:10:56 +0600

    javaScript Clock

--- setting.js
+++ setting.js
@@ -1,6 +1,28 @@
-const classCatch = document.getElementsByClassName("root");
+function showTime() {
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
 
-const h1Create = document.createElement("h1");
-h1Create.classList.add("headingTitle");
+  if (h == 0) {
+    h = 12;
+  }
 
-classCatch.appendChild(h1Create);
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+
+  setTimeout(showTime, 1000);
+}
+
+showTime();
